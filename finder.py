27743625a258b598eb3addb3a3dcdd8113aec8cb
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2022-12-02 19:51:04 +0000

    Revert "make multiple processes for more performance"
    This reverts commit 888ea42adc587322c236f5f097ee54b4dbdaa984.

--- finder.py
+++ finder.py
@@ -5,17 +5,16 @@ from mcstatus import JavaServer
 import argparse
 from utils import Logger
 
-
+# whre you want to startt on 1st quadrant
+START = 0
 
 REFRESH_RATE_TIMER = 1
 scanned = 1
 # how much you want to wait before closing connection
-SCAN_TIMEOUT = 2 
+SCAN_TIMEOUT = 5
 
 parser = argparse.ArgumentParser(description='files and stuff')
 parser.add_argument("-o","--outputfile", type=str, required=True, help="the name of the file to put in the results")
-parser.add_argument("-s","--start", type=int, required=True, help="start")
-parser.add_argument("-e","--end", type=int, required=True, help="end")
 args = parser.parse_args()
 
 OUTPUT_FILE = str(args.outputfile)
@@ -24,13 +23,7 @@ OUTPUT_PLAYER_WITH_SERVER_FILE = OUTPUT_FILE.split(".")[0]+"_found_players.txt"
 
 files = [OUTPUT_FILE, OUTPUT_RAW_IP_FILE, OUTPUT_PLAYER_WITH_SERVER_FILE]
 
-# whre you want to start/end on 1st quadrant
-START = args.start
-END = args.end
-
 logger = Logger('logs.txt')
-
-logger.addLog(f"New finder instance {START}:{END}")
 
 for file in files:
     try:
@@ -43,12 +36,11 @@ def ip_generator():
     # stop dinamicaly generating
     list = [i for i in range(256)]
     # if you want to change where you are scanning from
-    for i in range(START,END):
+    for i in range(START,256):
         for j in list:
             for p in list:
                 for k in list:
-                    yield "209.222.115.30"
-                    #yield f"{i}.{j}.{p}.{k}"
+                    yield f"{i}.{j}.{p}.{k}"
     #we ran all the ips
     #yield f"0.0.0.0"
 
@@ -57,7 +49,7 @@ ip_generator_object = ip_generator()
 # for measuing the rate
 def rate():
     # the amout of servers we are going to scan
-    amount_of_servers = (END-1-START)*255*255*255
+    amount_of_servers = (255-START)*255*255*255
     start = time.perf_counter()
     next_time = REFRESH_RATE_TIMER + SCAN_TIMEOUT
     while True:
@@ -71,14 +63,13 @@ def rate():
 threading.Thread(None, target=rate).start()
 
 def scan_ips():
-    status = JavaServer("0.0.0.0", 25565)
     # make socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(SCAN_TIMEOUT)
     while True:
         ip = next(ip_generator_object)
         try:
-            status = JavaServer(ip, 25565, timeout=SCAN_TIMEOUT).status()
+            sock.connect((ip, 25565))
         except:
             pass
         else:
@@ -87,13 +78,14 @@ def scan_ips():
                 file.write(f"{ip} \n")
                 file.close()
 
-            logger.addLog(f"{ip} with socket {status.description}")
+            logger.addLog(f"{ip} with socket")
+            sock.close()
     
         # for measuring performance
         global scanned
         scanned += 1
 
-threads_count = 10000
+threads_count = 5000
 for i in range(threads_count):
     #spawning a bunch of threads really doesnt matter how many
     threading.Thread(None, target=scan_ips).start()
